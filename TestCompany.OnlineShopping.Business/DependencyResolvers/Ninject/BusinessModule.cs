2b6c304ab8727446f0e22318987c5950282f37fb
Fatih Aksoy <aksoyfatih@outlook.com.tr>
2020-05-03 02:07:05 +0300

    Commit 4 - Dependecy Injection with Ninject IoC

---
+++ TestCompany.OnlineShopping.Business/DependencyResolvers/Ninject/BusinessModule.cs
@@ -0,0 +1,22 @@
+﻿using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestCompany.OnlineShopping.Business.Abstract;
+using TestCompany.OnlineShopping.Business.Concrete;
+using TestCompany.OnlineShopping.DataAccess.Abstract;
+using TestCompany.OnlineShopping.DataAccess.Concrete.EntityFramework;
+
+namespace TestCompany.OnlineShopping.Business.DependencyResolvers.Ninject
+{
+    public class BusinessModule:NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IProductService>().To<ProductManager>().InSingletonScope();
+            Bind<IProductDal>().To<EfProductDal>();
+
+            //Client side için bunun içinde bulunduğu klasöre ValidationModule diye bir class ekleyip işlem yapabilrisn.
+        }
+    }
+}
